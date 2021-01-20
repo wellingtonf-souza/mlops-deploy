be6b4701675e0dc1d13043ed45146a87bd775f3d
Wellington Ferreira <wellingtonf.souza@outlook.com>
2021-01-19 22:20:59 -0300

    first commit

---
+++ src/api/main.py
@@ -0,0 +1,42 @@
+
+from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
+from textblob import TextBlob
+import pickle as pkl
+import pandas as pd
+import os
+
+model = pkl.load(open('../../models/model.pkl','rb'))
+
+app = Flask(__name__)
+
+app.config['BASIC_AUTH_USERNAME'] = os.environ.get('BASIC_AUTH_USERNAME')
+app.config['BASIC_AUTH_PASSWORD'] = os.environ.get('BASIC_AUTH_PASSWORD')
+
+basic_auth = BasicAuth(app)
+
+@app.route('/')
+def home():
+    return 'API running'
+
+@app.route('/sentiment/<frase>/')
+def sentiment_analysis(frase):
+    tb = TextBlob(frase)
+    if tb.detect_language() != 'en':
+        tb_en = tb.translate(to='en')
+        polaridade = tb_en.sentiment.polarity 
+    else:   
+        polaridade = tb.sentiment.polarity
+    return 'polaridad: {}'.format(polaridade)
+
+@app.route('/house_price/',methods=['GET'])
+@basic_auth.required
+def house_price():
+    content = request.get_json()
+    df = [[content['tamanho'],content['ano'],content['garagem']]]
+    value = model.predict(df)
+    return jsonify({'value':float(value)})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
